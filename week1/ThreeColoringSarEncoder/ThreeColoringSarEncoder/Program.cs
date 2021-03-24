2a653a900ee5121f9cff4f1a3c6260aa4a014cba
Eetu Ikonen <eetu.e.ikonen@gmail.com>
2021-03-24 21:27:47 +0200

    Add k-coloring encoder

--- week1/ThreeColoringSarEncoder/ThreeColoringSarEncoder/Program.cs
+++ week1/ThreeColoringSarEncoder/ThreeColoringSarEncoder/Program.cs
@@ -9,6 +9,7 @@ namespace ThreeColoringSatEncoder {
     class Program {
         public const string CNF_PATH = "P:\\Stuff\\School\\alr_2021\\cnf";
         static void Main(string[] args) {
+
             Graph graph3b = new Graph(7,
                 Edge.FromChars('a', 'b'),
                 Edge.FromChars('a', 'c'),
@@ -36,10 +37,28 @@ namespace ThreeColoringSatEncoder {
                 Edge.FromChars('f', 'g'));
 
             ThreeColoringCNFSentence sentence3b = new ThreeColoringCNFSentence(graph3b);
+
             ThreeColoringCNFSentence sentence3c = new ThreeColoringCNFSentence(graph3c);
 
             SaveCNF("week1_3b", sentence3b.ToDIMACS());
             SaveCNF("week1_3c", sentence3c.ToDIMACS());
+        }
+
+        private static void Test2() {
+
+            Graph g = new Graph(3,
+                new Edge(0, 2),
+                new Edge(0, 1),
+                new Edge(1, 2));
+
+            ThreeColoringCNFSentence cnf = new ThreeColoringCNFSentence(g);
+            KColoringCNFSentence cnfK = new KColoringCNFSentence(g, 3);
+
+            Console.WriteLine(cnf.ToDIMACS());
+            Console.WriteLine();
+            Console.WriteLine(cnfK.ToDIMACS());
+
+            Console.ReadLine();
         }
 
         private static void SaveCNF(string filename, string cnf) {
--- week1/ThreeColoringSarEncoder/ThreeColoringSarEncoder/ThreeColoringCNFSentence.cs
+++ week1/ThreeColoringSarEncoder/ThreeColoringSarEncoder/ThreeColoringCNFSentence.cs
@@ -89,4 +89,83 @@ namespace ThreeColoringSatEncoder {
             return b.ToString();
         }
     }
+
+    public class KColoringCNFSentence {
+        private Graph graph;
+        public int VariableCount { get; private set; }
+        public List<int[]> Sentence { get; private set; } = new List<int[]>();
+        public int K { get; private set; }
+
+        public KColoringCNFSentence(Graph g, int k) {
+            this.K = k;
+            this.graph = g;
+            this.VariableCount = g.NodeCount * 3;
+            AddConstraintsForNodes();
+            AddConstraintsForEdges();
+        }
+
+        #region constraints
+        private void AddConstraintsForNodes() {
+            graph.ForEachNode(AddNodeConstraints);
+        }
+
+        private void AddNodeConstraints(int v) {
+            // At least 1 color
+            int[] atLeast1 = new int[K];
+            for (int i = 0; i < K; i++) {
+                for (int j = i + 1; j < K; j++) {
+                    AddClause(-GetColor(v, i), -GetColor(v, j));
+                }
+                atLeast1[i] = GetColor(v, i);
+            }
+
+            AddClause(atLeast1);
+        }
+
+        private void AddConstraintsForEdges() {
+            graph.ForEachEdge(AddEdgeConstraints);
+        }
+
+        private void AddEdgeConstraints(Edge e) {
+            // No edge from same color to same color
+            for (int i = 0; i < K; i++) {
+                AddClause(-GetColor(e.u, i), -GetColor(e.v, i));
+            }
+        }
+
+        public void AddClause(params int[] variables) {
+            Sentence.Add(variables);
+        }
+        #endregion
+
+        private int GetColor(int v, int color) {
+            return v * K + color + 1;
+        }
+
+        public override string ToString() {
+            StringBuilder b = new StringBuilder();
+            foreach (int[] clause in Sentence) {
+                b.Append("(");
+                foreach (int var in clause) {
+                    b.Append($"{var}, ");
+                }
+                b.Append(") &\n");
+            }
+            return b.ToString();
+        }
+
+        public string ToDIMACS() {
+            StringBuilder b = new StringBuilder();
+            b.Append($"p cnf {VariableCount} {Sentence.Count}\n");
+
+            foreach (int[] clause in Sentence) {
+                foreach (int var in clause) {
+                    b.Append($"{var} ");
+                }
+                b.Append("0\n");
+            }
+
+            return b.ToString();
+        }
+    }
 }
