b1372864ba23e5c4528be00ae90332548b417cfd
Eetu Ikonen <eetu.e.ikonen@gmail.com>
2021-03-31 02:21:56 +0300

    asd

--- week2/Exercise2_Sudoku/Exercise2_Sudoku/SudokuEncoderLadder.cs
+++ week2/Exercise2_Sudoku/Exercise2_Sudoku/SudokuEncoderLadder.cs
@@ -17,19 +17,21 @@ namespace Exercise2_Sudoku {
 
             AddFixedConstraints();
             AddRowAndColumnConstraints();
-            // AddGridConstraints();
+            AddGridConstraints();
         }
 
         #region fixed
         private void AddFixedConstraints() {
-            for (int i = 0; i < sudoku.N2; i++) {
-                for (int j = 0; j < sudoku.N2; j++) {
+            for (int row = 0; row < sudoku.N2; row++) {
+                for (int col = 0; col < sudoku.N2; col++) {
+
                     // from 1 based to 0 based
-                    int val = sudoku.Table[i, j] - 1;
+                    int val = sudoku.Table[row, col] - 1;
                     if (val >= 0) {
-                        // from 0 based back to 1 based
-                        AddClause(VarIndex(i, j, val));
+                        // from 0 based to 1 based variable index
+                        AddClause(VarIndex(row, col, val));
                     }
+
                 }
             }
         }
@@ -39,27 +41,48 @@ namespace Exercise2_Sudoku {
         private void AddRowAndColumnConstraints() {
             // Methods separated for clarity, could combine loops for optimal performance
             AtLeastOneInRowAndColumn();
-            AtMostInOneRow_Ladder();
-            //AtMostInOneCol_Ladder();
 
-            AtLeastOnePerCell();
-            AtMostOnePerCell();
+            //AtMostOneInRowAndCol_Pairwise();
+            AtMostOneInRow_Ladder();
+            AtMostOneInCol_Ladder();
+
+            ExactlyOnePerCell_Pairwise();
         }
 
+
         private void AtLeastOneInRowAndColumn() {
-            for (int i = 0; i < sudoku.N2; i++) {
+            for (int row = 0; row < sudoku.N2; row++) {
                 for (int val = 0; val < sudoku.N2; val++) {
 
                     int[] rowClause = new int[sudoku.N2];
                     int[] colClause = new int[sudoku.N2];
-                    for (int j = 0; j < sudoku.N2; j++) {
-                        rowClause[j] = VarIndex(i, j, val);
-                        colClause[j] = VarIndex(j, i, val);
+                    for (int col = 0; col < sudoku.N2; col++) {
+                        rowClause[col] = VarIndex(row, col, val);
+                        colClause[col] = VarIndex(col, row, val);
                     }
                 }
             }
         }
-        private void AtMostInOneRow_Ladder() {
+
+        private void AtMostOneInRowAndCol_Pairwise() {
+
+            for (int row = 0; row < sudoku.N2; row++) {
+                for (int val = 0; val < sudoku.N2; val++) {
+
+                    for (int col1 = 0; col1 < sudoku.N2; col1++) {
+                        for (int col2 = 0; col2 < sudoku.N2; col2++) {
+                            if (col1 == col2) {
+                                continue;
+                            }
+
+                            AddClause(-VarIndex(row, col1, val), -VarIndex(row, col2, val));
+                            AddClause(-VarIndex(col1, row, val), -VarIndex(col2, row, val));
+                        }
+                    }
+                }
+            }
+        }
+        private void AtMostOneInRow_Ladder() {
 
             for (int row = 0; row < sudoku.N2; row++) {
                 for (int val = 0; val < sudoku.N2; val++) {
@@ -75,8 +98,7 @@ namespace Exercise2_Sudoku {
                 }
             }
         }
-
-        private void AtMostInOneCol_Ladder() {
+        private void AtMostOneInCol_Ladder() {
 
             for (int j = 0; j < sudoku.N2; j++) {
                 for (int val = 0; val < sudoku.N2; val++) {
@@ -89,6 +111,41 @@ namespace Exercise2_Sudoku {
                     }
 
                     ladderVarCount += sudoku.N2 - 1;
+                }
+            }
+        }
+
+
+        private void ExactlyOnePerCell_Pairwise() {
+            for (int row = 0; row < sudoku.N2; row++) {
+                for (int col = 0; col < sudoku.N2; col++) {
+
+                    List<int> clause = new List<int>();
+
+                    for (int val1 = 0; val1 < sudoku.N2; val1++) {
+                        clause.Add(VarIndex(row, col, val1));
+                        for (int val2 = 0; val2 < sudoku.N2; val2++) {
+                            if (val1 == val2) {
+                                continue;
+                            }
+
+                            AddClause(-VarIndex(row, col, val1), -VarIndex(row, col, val2));
+                        }
+                    }
+
+                    AddClause(clause.ToArray());
+                }
+            }
+        }
+
+        private void AtLeastOnePerCell() {
+            for (int row = 0; row < sudoku.N2; row++) {
+                for (int col = 0; col < sudoku.N2; col++) {
+                    List<int> clause = new List<int>();
+                    for (int val = 0; val < sudoku.N2; val++) {
+                        clause.Add(VarIndex(row, col, val));
+                    }
+                    AddClause(clause.ToArray());
                 }
             }
         }
@@ -112,28 +169,14 @@ namespace Exercise2_Sudoku {
                     AddClause(clause.ToArray());
                 }
             }
-        }
-
-        private void AtLeastOnePerCell() {
-            for (int row = 0; row < sudoku.N2; row++) {
-                for (int col = 0; col < sudoku.N2; col++) {
-                    List<int> clause = new List<int>();
-                    for (int val = 0; val < sudoku.N2; val++) {
-                        clause.Add(VarIndex(row, col, val));
-                    }
-                    AddClause(clause.ToArray());
-                }
-            }
-        }
-
-
+        }    
         #endregion
 
         #region grid
         private void AddGridConstraints() {
             // Methods separated for clarity, could combine loops for optimal performance
             AtLeastOneInGrid();
-            AtMostOneInGrid_Ladder();
+            AtMostOneInGrid_Pairwise();
         }
 
         private void AtLeastOneInGrid() {
@@ -153,6 +196,27 @@ namespace Exercise2_Sudoku {
             }
         }
 
+        private void AtMostOneInGrid_Pairwise() {
+            for (int grid = 0; grid < sudoku.N2; grid++) {
+                for (int val = 0; val < sudoku.N2; val++) {
+
+                    // Pairwise coordinates
+                    for (int i1 = 0; i1 < sudoku.N; i1++) {
+                        for (int j1 = 0; j1 < sudoku.N; j1++) {
+                            for (int i2 = 0; i2 < sudoku.N; i2++) {
+                                for (int j2 = 0; j2 < sudoku.N; j2++) {
+                                    if (i1 == i2 && j1 == j2) {
+                                        continue;
+                                    }
+
+                                    AddClause(-VarGridIndex(grid, i1, j1, val), -VarGridIndex(grid, i2, j2, val));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         private void AtMostOneInGrid_Ladder() {
             for (int grid = 0; grid < sudoku.N2; grid++) {
                 for (int val = 0; val < sudoku.N2; val++) {
@@ -175,7 +239,18 @@ namespace Exercise2_Sudoku {
 
         #region utility
         private int VarIndex(int x, int y, int value) {
-            return x * sudoku.N2 * sudoku.N2 + y * sudoku.N2 + value + 1;
+            int var = x * sudoku.N2 * sudoku.N2 + y * sudoku.N2 + value + 1;
+
+            Program.SetIndices(sudoku.N2, var, out int backX, out int backY, out int backValue);
+
+            if (x != backX || y != backY || value != backValue) {
+                Console.WriteLine($"Original: ({x}, {y}) = {value}");
+                Console.WriteLine($"Variable: {var}");
+                Console.WriteLine($"Back    : ({backX}, {backY}) = {backValue}");
+                throw new Exception("Invalid conversion");
+            }
+
+            return var;
         }
         private int VarGridIndex(int grid, int x, int y, int val) {
             int gridX = grid / sudoku.N;
